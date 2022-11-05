c7b94b358c66fd795f6d604501ba18309f63150b
Be-student <presentsong@naver.com>
2022-11-05 21:41:43 +0900

    feat: Controller에서 종료 메시지 수신 기능 추가

---
+++ src/main/java/baseball/domain/ControllerInput.java
@@ -0,0 +1,28 @@
+package baseball.domain;
+
+import baseball.config.ControllerConstants;
+import baseball.view.InputView;
+
+public class ControllerInput {
+    private static final String CONTROLLER_ERROR_MESSAGE = "1,2의 입력만 들어와야 합니다";
+    private final InputView inputView;
+
+    private ControllerInput(InputView inputView) {
+        this.inputView = inputView;
+    }
+
+    public static ControllerInput from(InputView inputView) {
+        return new ControllerInput(inputView);
+    }
+
+    public ControllerConstants getInput() {
+        try {
+            String inputLine = inputView.readLine();
+            return ControllerConstants.from(Integer.parseInt(inputLine));
+        } catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+            throw new IllegalArgumentException(CONTROLLER_ERROR_MESSAGE, e);
+        }
+
+    }
+}
