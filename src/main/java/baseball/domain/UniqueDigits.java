fcf04742b26eae72ffceb4d1191f9fc2aca62926
Be-student <presentsong@naver.com>
2022-11-05 20:27:41 +0900

    feat: 모든 자리수가 다른 일급 컬렉션 추가

---
+++ src/main/java/baseball/domain/UniqueDigits.java
@@ -0,0 +1,48 @@
+package baseball.domain;
+
+import org.assertj.core.util.Sets;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static baseball.config.GameConstants.MAX_INPUT_INCLUSIVE;
+import static baseball.config.GameConstants.MIN_INPUT_INCLUSIVE;
+
+public class UniqueDigits {
+    private static final String OVER_RANGE_MESSAGE = "입력이 999를 초과했습니다";
+    private static final String LESS_RANGE_MESSAGE = "입력이 100보다 작았습니다";
+    private static final String DUPLICATE_INPUT_MESSAGE = "입력에 중복값이 있습니다";
+    private List<Integer> uniqueDigits = new ArrayList<>();
+
+    private UniqueDigits(int uniqueNumber) {
+        isDigitInRange(uniqueNumber);
+        this.uniqueDigits = splitDigits(uniqueNumber);
+    }
+
+    public static UniqueDigits of(int digits) {
+        return new UniqueDigits(digits);
+    }
+
+    private void isDigitInRange(int uniqueNumber) {
+        if (uniqueNumber > MAX_INPUT_INCLUSIVE) {
+            throw new IllegalArgumentException(OVER_RANGE_MESSAGE);
+        }
+        if (uniqueNumber < MIN_INPUT_INCLUSIVE) {
+            throw new IllegalArgumentException(LESS_RANGE_MESSAGE);
+        }
+    }
+
+    private List<Integer> splitDigits(int uniqueNumber) {
+        List<Integer> digits = new ArrayList<>();
+
+        while (uniqueNumber > 0) {
+            digits.add(0, uniqueNumber % 10);
+            uniqueNumber /= 10;
+        }
+
+        if (Sets.newHashSet(digits).size() != 3) {
+            throw new IllegalArgumentException(DUPLICATE_INPUT_MESSAGE);
+        }
+        return digits;
+    }
+}
---
+++ src/test/java/baseball/domain/UniqueDigitsTest.java
@@ -0,0 +1,41 @@
+package baseball.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class UniqueDigitsTest {
+    @DisplayName("잘 작동하는 경우 체크")
+    @Test
+    void 잘_작동함() {
+        assertDoesNotThrow(() -> {
+            UniqueDigits.of(123);
+        });
+    }
+
+    @DisplayName("1000이상의 숫자일 때 에러")
+    @Test
+    void 최대_초과() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            UniqueDigits.of(1000);
+        });
+    }
+
+    @DisplayName("1000이상의 숫자일 때 에러")
+    @Test
+    void 최소_범위_초과() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            UniqueDigits.of(98);
+        });
+    }
+
+    @DisplayName("중복이 있는 숫자일 경우 에러를 만듦")
+    @Test
+    void 중복_숫자() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            UniqueDigits.of(112);
+        });
+    }
+}
