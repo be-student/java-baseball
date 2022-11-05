aef2aec6bca3e15a8a309bf8677d06f5348d2195
Be-student <presentsong@naver.com>
2022-11-05 22:47:04 +0900

    feat: RandomAnswer 테스트 추가

---
+++ src/test/java/baseball/domain/RandomAnswerTest.java
@@ -0,0 +1,14 @@
+package baseball.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+public class RandomAnswerTest {
+    @DisplayName("Random 값을 생성할 수 있음")
+    @Test
+    void 잘_작동함() {
+        assertDoesNotThrow(() -> RandomAnswer.createRandom());
+    }
+}
