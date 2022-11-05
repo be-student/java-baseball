3567e6adfaed336f5f075d019d28b723c26b5624
Be-student <presentsong@naver.com>
2022-11-06 00:28:09 +0900

    feat: 컨트롤러 작동 코드 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,16 @@
 package baseball;
 
+import baseball.domain.BaseballController;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+import baseball.view.console.ConsoleInput;
+import baseball.view.console.ConsoleOutput;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = ConsoleInput.getInstance();
+        OutputView outputView = ConsoleOutput.getInstance();
+        BaseballController.from(inputView, outputView).run();
     }
 }
