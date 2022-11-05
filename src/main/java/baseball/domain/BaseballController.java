2c034e14087328b6fd65e5afe4f83ff7ceb89926
Be-student <presentsong@naver.com>
2022-11-06 00:28:01 +0900

    feat: 게임 컨트롤러 추가

---
+++ src/main/java/baseball/domain/BaseballController.java
@@ -0,0 +1,42 @@
+package baseball.domain;
+
+import baseball.config.ControllerConstants;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
+public class BaseballController {
+    private InputView inputView;
+    private OutputView outputView;
+
+    private BaseballController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public static BaseballController from(InputView inputView, OutputView outputView) {
+        return new BaseballController(inputView, outputView);
+    }
+
+    public void run() {
+        initController();
+        while (true) {
+            Game.of(inputView, outputView).run();
+            printRetryMessage();
+            if (ControllerInput.from(inputView).getInput() == ControllerConstants.FINISH) {
+                return;
+            }
+        }
+    }
+
+    private void initController() {
+        printInitMessage();
+    }
+
+    private void printInitMessage() {
+        outputView.print("숫자 야구 게임을 시작합니다.\n");
+    }
+
+    private void printRetryMessage() {
+        outputView.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
+    }
+}
