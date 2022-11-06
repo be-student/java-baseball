2e68d7d6dacc1858512d5620f4808ee890421ddb
Be-student <presentsong@naver.com>
2022-11-06 12:37:02 +0900

    test: randomInRange 값을 잘 받아서 계산하는 테스트 추가

--- src/test/java/baseball/game/RandomAnswerTest.java
+++ src/test/java/baseball/game/RandomAnswerTest.java
@@ -3,6 +3,9 @@ package baseball.game;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Constructor;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 public class RandomAnswerTest {
@@ -10,5 +13,25 @@ public class RandomAnswerTest {
     @Test
     void 잘_작동함() {
         assertDoesNotThrow(RandomAnswer::createRandom);
+    }
+
+    @DisplayName("RandomInRange를 주입한다")
+    @Test
+    void randomInRange() throws NoSuchMethodException {
+        Constructor constructor = RandomAnswer.class.getConstructor(RandomInRangeImpl.class);
+        constructor.setAccessible(true);
+        RandomInRangeImpl randomInRange = new RandomInRangeImpl() {
+            private int temp = 1;
+
+            @Override
+            public int pickInRange(int startInclusive, int endInclusive) {
+                return temp++;
+            }
+        };
+
+        RandomAnswer test = new RandomAnswer(randomInRange);
+        assertThat(test.digitWithIndex(0)).isEqualTo(1);
+        assertThat(test.digitWithIndex(1)).isEqualTo(2);
+        assertThat(test.digitWithIndex(2)).isEqualTo(3);
     }
 }
