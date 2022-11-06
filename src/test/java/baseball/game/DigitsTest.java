9dffe5304d412c7eda91c2e1ab441c9f86523f6c
Be-student <presentsong@naver.com>
2022-11-06 13:27:28 +0900

    test: 숫자 컬렉션 테스트 추가

---
+++ src/test/java/baseball/game/DigitsTest.java
@@ -0,0 +1,25 @@
+package baseball.game;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DigitsTest {
+    @DisplayName("Digits 일급 컬렉션이 잘 작동함")
+    @Test
+    void 잘_작동() {
+        Digits digits = new Digits();
+        assertThat(digits.size()).isEqualTo(0);
+        Digit ZERO = Digit.from(0);
+
+        digits.add(ZERO);
+        assertThat(digits.size()).isEqualTo(1);
+
+        assertThat(digits.contains(ZERO)).isEqualTo(true);
+        assertThat(digits.size()).isEqualTo(1);
+
+        digits.add(ZERO);
+        assertThat(digits.size()).isEqualTo(2);
+    }
+}
