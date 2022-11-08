0637b96e69fef1a2b332a91dd4fbd11af27aa5ef
Be-student <presentsong@naver.com>
2022-11-08 12:22:51 +0900

    refactor: 쓸모 없는 주석 제거

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,7 +7,6 @@ public class Application {
         // TODO: 프로그램 구현
         //Todo Input 여기다 view 주입
         //Todo new 를 사용 줄이기
-        //
         BaseballController baseballController = new BaseballController();
         baseballController.run();
     }
