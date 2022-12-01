4bc7014a162b72d92f55d174dc70acdb78297a8d
Be-student <presentsong@naver.com>
2022-12-01 21:24:09 +0900

    feat: playerCommand에 string부터 변경하도록 추가

--- src/main/java/baseball/application/port/in/GamePlayCommand.java
+++ src/main/java/baseball/application/port/in/GamePlayCommand.java
@@ -1,25 +1,36 @@
 package baseball.application.port.in;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class GamePlayCommand {
 
     private static final String NULL_MESSAGE = "입력에 null 이 있으면 안 됩니다";
+    private static final String NOT_NUMBER_MESSAGE = "숫자로 변환할 수 없습니다";
+    private static final Pattern NUMBER = Pattern.compile("^\\d+$");
 
     private final List<Integer> playerInput;
 
-    public GamePlayCommand(List<Integer> playerInput) {
-        this.playerInput = playerInput;
-        validate();
+    public GamePlayCommand(String input) {
+        validate(input);
+        playerInput = Arrays.stream(input.split(""))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 
-    private void validate() {
-        if (playerInput == null) {
+    private void validate(String input) {
+        if (input == null) {
             throw new IllegalArgumentException(NULL_MESSAGE);
         }
-        if (playerInput.contains(null)) {
-            throw new IllegalArgumentException(NULL_MESSAGE);
+        if (isNotNumber(input)) {
+            throw new IllegalArgumentException(NOT_NUMBER_MESSAGE);
         }
+    }
+
+    private boolean isNotNumber(String input) {
+        return !NUMBER.matcher(input).matches();
     }
 
 
