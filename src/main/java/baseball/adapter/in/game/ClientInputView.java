f2109b007420b101fe80263f81cd27631838f052
Be-student <presentsong@naver.com>
2022-12-01 21:23:45 +0900

    feat: inputView 추가

---
+++ src/main/java/baseball/adapter/in/game/ClientInputView.java
@@ -0,0 +1,10 @@
+package baseball.adapter.in.game;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class ClientInputView {
+
+    public String readLine() {
+        return Console.readLine();
+    }
+}
