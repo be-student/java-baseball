01bed130f44cc0d3d8c66404c817ef12c293664c
Be-student <presentsong@naver.com>
2022-12-01 21:23:13 +0900

    fix: 선택을 string으로 바로 비교하도록 변경

--- src/main/java/baseball/adapter/in/game/ClientChoice.java
+++ src/main/java/baseball/adapter/in/game/ClientChoice.java
@@ -1,26 +1,23 @@
 package baseball.adapter.in.game;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 public enum ClientChoice {
-    RETRY(1),
-    QUIT(2);
+    RETRY("1"),
+    QUIT("2");
 
     private static final String NOT_FOUND_ERROR = "1,2만 입력하셔야 합니다";
-    private final int value;
+    private final String value;
 
-    ClientChoice(int value) {
+    ClientChoice(String value) {
         this.value = value;
     }
 
-    public static ClientChoice from(int value) {
+    public static ClientChoice from(String value) {
         return Arrays.stream(ClientChoice.values())
-                .filter(it -> it.value == value)
+                .filter(it -> Objects.equals(it.value, value))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException(NOT_FOUND_ERROR));
-    }
-
-    public int getValue() {
-        return value;
     }
 }
